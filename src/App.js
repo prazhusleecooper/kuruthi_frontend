9724783c3b7290b4cb24e91b5d655f2c118d6cde
belikeparthaa <sarathy10499@gmail.com>
2021-01-31 12:06:23 +0530

    partha-push

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from "react";
 
 import "./App.css";
 
-import ProfilePage from "./Pages/ProfilePage";
 
 class App extends React.Component {
 	render() {
-		// return <div>KURUTHI REBOOT</div>;
-		// return <ProfilePage />;
-		return <div>hello</div>;
+		return <div>KURUTHI REBOOT</div>;
 	}
 }
 
