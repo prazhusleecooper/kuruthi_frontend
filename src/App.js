2bc54c0249cddf853252e7694d0d448d30112de3
Prashanth-WH <prashanth@thinkcodesoft.com>
2021-01-31 11:45:58 +0530

    sample push

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from "react";
 
 import "./App.css";
 
+import ProfilePage from "./Pages/ProfilePage";
+
 class App extends React.Component {
 	render() {
-		return <div>KURUTHI REBOOT</div>;
+		// return <div>KURUTHI REBOOT</div>;
+		// return <ProfilePage />;
+		return <div>hello</div>;
 	}
 }
 
