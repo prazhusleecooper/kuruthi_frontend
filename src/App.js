755d7b53db842c6d062dc0a28f17b16835e00413
gokulkavin <gokulkavin1998@gmail.com>
2021-01-31 12:14:03 +0530

    Gokul Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import "./App.css";
 
 class App extends React.Component {
 	render() {
-		return <div>KURUTHI REBOOT</div>;
+		// return <div>KURUTHI REBOOT</div>;
+		// return <ProfilePage />;
+		return <div>hello Gokul</div>;
+
 	}
 }
 
